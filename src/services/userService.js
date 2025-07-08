1af82059557b39ecd6680eb2b76f491939d3ed09
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2025-07-09 00:14:12 +0700

    refactor(userService): migrate addUser to use SQLite instead of in-memory array

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,18 +1,23 @@
+const db = require('../db/database');
+const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
-
-const users = [];
 
 const addUser = async ({ username, password }) => {
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = {
-    id: `${Date.now()}`,
-    username,
-    password: hashedPassword,
-  };
-  users.push(newUser);
-  return newUser;
+  const id = uuidv4();
+  const stmt = db.prepare(`INSERT INTO users (id, username, password) VALUES (?, ?, ?)`);
+  stmt.run(id, username, hashedPassword);
+  return { id, username };
 };
 
-const getUserByUsername = (username) => users.find((u) => u.username === username);
+const getUserByUsername = (username) => {
+  const stmt = db.prepare(`SELECT * FROM users WHERE username = ?`);
+  return stmt.get(username);
+};
 
-module.exports = { addUser, getUserByUsername };
+const getUserById = (id) => {
+  const stmt = db.prepare(`SELECT * FROM users WHERE id = ?`);
+  return stmt.get(id);
+};
+
+module.exports = { addUser, getUserByUsername, getUserById };
