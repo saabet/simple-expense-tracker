e3b739c59bb6c454020f03a8862dbe616e5903be
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2025-07-10 12:01:37 +0700

    feat(token): resolve error finding secret file

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 
 const generateToken = (user) => {
   const payload = { id: user.id, username: user.username };
-  const secret = process.env.JWT_SECRET;
   if (!secret) throw new Error('JWT_SECRET is not defined in environment variables');
   return jwt.sign(payload, secret, { expiresIn: '1d' });
 };
